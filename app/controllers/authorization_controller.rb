77ca91828b0c04849dd9191b9f219ac0048056e6
Noah Lively <noah@koremedia.net>
2012-08-29 14:24:36 -0700

    Initial commit

---
+++ app/controllers/authorization_controller.rb
@@ -0,0 +1,7 @@
+class AuthorizationController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthServer::Application.config.secret_token = '91da6c6162841ef8c6bd510836485f4b08dfd36681a673b992dcbd7bb845c4bfa62d7997442c1d2ad243ffa2417b211eead9d38b93e8c1aea04171c72ecf9d89'
