4a76dcab050339dad6cc1c46f62c30ae5870326d
Noah Lively <noah@koremedia.net>
2012-09-07 18:33:38 -0700

    Added checkin model and controller

---
+++ app/controllers/check_ins_controller.rb
@@ -0,0 +1,23 @@
+class CheckInsController < ApplicationController
+  def create
+
+    @checkin = CheckIn.create! :user_id => params[:user_id], :latitude => params[:latitude], :longitude => params[:longitude]
+
+    respond_to do |format|
+      format.html { redirect_to 'index' }
+      format.json { render json: @checkin }
+    end
+
+  end
+
+  def index
+
+    @checkins = CheckIn.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @checkins }
+    end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906231518) do
+ActiveRecord::Schema.define(:version => 20120907224913) do
+
+  create_table "check_ins", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "latitude"
+    t.integer  "longitude"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "locations", :force => true do |t|
     t.string   "street"
