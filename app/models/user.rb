6e85b960a68e49868c7051c424b05a74d29efa81
Noah Lively <noah@koremedia.net>
2012-09-10 22:33:23 -0700

    finished getting it to work on the server end

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+
+  def self.authenticate!(username, password)
+    return nil if username.blank? || password.blank?
+
+    if (user = User.find_by_email(username))
+      return user if user.valid_password?(password)
+    end
+
+    nil
+  end
+
 end
