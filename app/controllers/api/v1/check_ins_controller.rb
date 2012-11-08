b162fbd757ad2de9de57719485188c9710e9b84c
Noah Lively <noah@koremedia.net>
2012-11-08 15:38:41 -0800

    updates to checkin/out controller

--- app/controllers/api/v1/check_ins_controller.rb
+++ app/controllers/api/v1/check_ins_controller.rb
@@ -7,7 +7,15 @@ module Api::V1
 
     def create
 
-      @checkin = CheckIn.create! :user_id => params[:user_id], :latitude => params[:latitude], :longitude => params[:longitude]
+      @checkin = CheckIn.new :user_id => params[:user_id], :latitude => params[:latitude], :longitude => params[:longitude], :in_out => params[:in_out]
+
+      if @checkin.in_out
+        @checkin.session_guid = UUID.generate
+      else
+        @checkin.session_guid = params[:session_guid]
+      end
+
+      @checkin.save!
 
       respond_to do |format|
         format.html { redirect_to 'index' }
