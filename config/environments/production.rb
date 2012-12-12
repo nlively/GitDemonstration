384749707e73eb5fb20dc1c47a0be1fe50f5d5b6
Noah Lively <noah@koremedia.net>
2012-12-11 16:04:29 -0800

    more asset stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ OauthServer::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.initialize_on_precompile = false
 
@@ -48,7 +48,7 @@ OauthServer::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  #config.assets.precompile += %w( modernizr-2.6.2.min.js main.js plugins.js )
+  config.assets.precompile += %w( modernizr-2.6.2.min.js main.js plugins.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
