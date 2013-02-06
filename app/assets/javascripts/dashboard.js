c84fa166084fbbefc151185144ea3125f530a59a
Noah Lively <noah@koremedia.net>
2013-02-06 09:58:37 -0800

    clean & precompile assets

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -8,7 +8,7 @@ $(function(){
     $('.main-col').equalHeights(600, 3000);
   });
 
-  if ($('body.dashboard.invoices.index').length > 0) {
+  if ($('body.dashboard.invoices.index, body.dashboard.batches.index').length > 0) {
     var selector = $('.filter-field.selector select');
 
     function updateFieldSelectors() {
@@ -18,13 +18,21 @@ $(function(){
         case 'client_name':
           $('.filter-field.client').css('display', 'inline-block');
           break;
+        case 'employee_name':
+          $('.filter-field.employee').css('display', 'inline-block');
+          break;
         case 'invoice_number':
           $('.filter-field.invoice-number').css('display', 'inline-block');
           break;
+        case 'batch_number':
+          $('.filter-field.batch-number').css('display', 'inline-block');
+          break;
         case 'invoice_date':
+        case 'batch_date':
           $('.filter-field.date').css('display', 'inline-block');
           break;
         case 'invoice_status':
+        case 'batch_status':
           $('.filter-field.status').css('display', 'inline-block');
           break;
       }
