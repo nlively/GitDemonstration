1dd8763ed35fe0fb091f97d4f9f1b02e82d1fd4f
Noah Lively <noah@koremedia.net>
2013-02-08 08:44:36 -0800

    custom login screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout_by_resource
+
+    def layout_by_resource
+      if devise_controller?
+        "splash"
+      else
+        "application"
+      end
+    end
+
+  before_filter :authenticate_user!
   after_filter :store_last_destination
 
 
