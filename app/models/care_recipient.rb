9bd9380017f167919f411e7314721bc60088517d
Noah Lively <noah@koremedia.net>
2013-05-24 08:04:37 -0700

    care recipient fixes

--- app/models/care_recipient.rb
+++ app/models/care_recipient.rb
@@ -31,7 +31,7 @@ class CareRecipient < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  #attr_accessible :email, :password, :password_confirmation, :remember_me
   include ActionView::Helpers::NumberHelper
   include ResourcesHelper
 
